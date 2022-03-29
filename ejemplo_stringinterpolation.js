7fcb7c34e6e16ed7f46854087f855cab2d7f7f28
Coco <91480391+Neri-hV@users.noreply.github.com>
2022-03-29 10:33:36 -0400

    Add files via upload

---
+++ ejemplo_stringinterpolation.js
@@ -0,0 +1,10 @@
+
+var a = 5
+var b = 3
+console.log('Pedro tiene ' + (a + b) + ' años');
+
+//es lo mismo que : usar
+console.log(`Pedro tiene ${a + b} años`);
+
+// ver mas usos del ${} en https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#string_interpolation
+
